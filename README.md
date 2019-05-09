7655c660419a51b11e7ad66af1593a420dd91095
Jason <jasontcrabtree@gmail.com>
2019-05-09 18:21:57 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Seed: Small Code Snippets to help with HTML & CSS
+
+This project was initially built as part of the FreeCodeCamp Responsive Web Design Certificate.
+
+Building this project was a great chance to play around with making a small Design System — and record useful Code Snippets I repeatedly use across different projects.
+
+Feel free to borrow, copy or re-use anything you find here.
+
+This is all open source.
---
+++ script.js
@@ -0,0 +1,35 @@
+(function() {
+	"use strict";
+
+	var sections = document.querySelectorAll("article [id]");
+	var navLinks = document.querySelectorAll("nav ul li a");
+
+	function getClosestSection() {
+		var sectionsLength = sections.length;
+
+		for (var index = 0; index < sectionsLength; index++) {
+			if (isBelowScroll(sections.item(index))) break;
+		}
+
+		selectLink(sections.item(index).id);
+	}
+
+	function isBelowScroll(element) {
+		var position = element.getBoundingClientRect();
+		return position.top > 0;
+	}
+
+	function selectLink(id) {
+		Array.prototype.forEach.call(navLinks, function(element) {
+			element.classList.remove("is-selected");
+		});
+
+		document
+			.querySelector('a[href="#' + id + '"]')
+			.classList.add("is-selected");
+	}
+
+	window.addEventListener("scroll", function(event) {
+		getClosestSection();
+	});
+})();
